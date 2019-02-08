89723107219271f1e936ee9a56c0c64115eae749
Michel van der Hulst <michel@voorkanter.com>
2019-02-08 17:30:23 +0100

    Add routing to frontend

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,6 +1,7 @@
-module Main exposing (..)
+module Main exposing (Actions, DatabaseState(..), Model, Msg(..), Page(..), WorkDay, WorkSheet, actionsDecoder, filesDecoder, init, main, subscriptions, update, uploadView, view, viewAction, viewActions, viewDay, viewDuration, viewWorkSheet, workDayDecoder, workSheetDecoder)
 
 import Browser
+import Browser.Navigation exposing (Key)
 import Dict exposing (Dict)
 import File exposing (File)
 import Html exposing (..)
@@ -8,14 +9,19 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode as D
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s)
+
 
 
 -- MAIN
 
 
 main =
-    Browser.element
+    Browser.application
         { init = init
+        , onUrlChange = OnUrlChange
+        , onUrlRequest = OnUrlRequest
         , view = view
         , update = update
         , subscriptions = subscriptions
@@ -38,16 +44,91 @@ type alias WorkSheet =
     Dict String WorkDay
 
 
-type Model
+type alias Model =
+    { page : Page
+    , key : Key
+    }
+
+
+type Page
+    = UploadingDatabase DatabaseState
+    | Day String
+    | AvailableDays
+    | PageNone
+
+
+type Route
+    = Upload
+    | ViewDay String
+    | SelectAvailableDay
+    | NotFound
+
+
+type DatabaseState
     = Waiting
     | Uploading Float
     | Done WorkSheet
     | Fail
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Waiting
+parseUrl : Url -> Route
+parseUrl url =
+    case Parser.parse routeParser url of
+        Just route ->
+            route
+
+        Nothing ->
+            NotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ Parser.map ViewDay (s "dag" </> Parser.string)
+        , Parser.map Upload (s "upload-registratie")
+        , Parser.map SelectAvailableDay (s "kies-een-dag")
+        ]
+
+
+getPath : Route -> String
+getPath route =
+    case route of
+        NotFound ->
+            "/404"
+
+        Upload ->
+            "/upload-registratie"
+
+        SelectAvailableDay ->
+            "/kies-een-dag"
+
+        ViewDay day ->
+            "/dag/" ++ day
+
+
+getPage : Route -> Page
+getPage route =
+    case route of
+        NotFound ->
+            PageNone
+
+        Upload ->
+            UploadingDatabase Waiting
+
+        SelectAvailableDay ->
+            AvailableDays
+
+        ViewDay day ->
+            Day day
+
+
+init : () -> Url -> Key -> ( Model, Cmd Msg )
+init _ url key =
+    let
+        page =
+            parseUrl url |> getPage
+    in
+    ( { page = page, key = key }
     , Cmd.none
     )
 
@@ -75,16 +156,19 @@ type Msg
     = GotFiles (List File)
     | GotProgress Http.Progress
     | Uploaded (Result Http.Error WorkSheet)
+    | OnUrlRequest Browser.UrlRequest
+    | OnUrlChange Url
+    | Noop
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotFiles files ->
-            ( Uploading 0
+            ( { model | page = UploadingDatabase (Uploading 0) }
             , Http.request
                 { method = "POST"
-                , url = "/upload"
+                , url = "http://localhost:3010/upload"
                 , headers = []
                 , body = Http.multipartBody (List.map (Http.filePart "file") files)
                 , expect = Http.expectJson Uploaded workSheetDecoder
@@ -96,7 +180,7 @@ update msg model =
         GotProgress progress ->
             case progress of
                 Http.Sending p ->
-                    ( Uploading (Http.fractionSent p), Cmd.none )
+                    ( { model | page = UploadingDatabase (Uploading (Http.fractionSent p)) }, Cmd.none )
 
                 Http.Receiving _ ->
                     ( model, Cmd.none )
@@ -104,10 +188,32 @@ update msg model =
         Uploaded result ->
             case result of
                 Ok response ->
-                    ( Done response, Cmd.none )
+                    ( { model | page = UploadingDatabase (Done response) }, Cmd.none )
 
                 Err _ ->
-                    ( Fail, Cmd.none )
+                    ( { model | page = UploadingDatabase Fail }, Cmd.none )
+
+        OnUrlChange url ->
+            let
+                newPage =
+                    parseUrl url |> getPage
+            in
+            ( { model | page = newPage }, Cmd.none )
+
+        Noop ->
+            ( model, Cmd.none )
+
+        OnUrlRequest urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model
+                    , Browser.Navigation.pushUrl model.key (Url.toString url)
+                    )
+
+                Browser.External url ->
+                    ( model
+                    , Browser.Navigation.load url
+                    )
 
 
 
@@ -123,9 +229,53 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+viewNavBar : Html Msg
+viewNavBar =
+    Html.nav [ class "navbar navbar-expand-lg navbar-dark bg-dark" ]
+        [ Html.a [ class "navbar-brand" ] [ text "Humako uren registratie" ]
+        , div [ class "collapse navbar-collapse show" ]
+            [ Html.ul [ class "navbar-nav" ]
+                [ Html.li
+                    [ class "nav-item" ]
+                    [ Html.a [ class "nav-link", href (getPath Upload) ] [ text "Upload nieuwe registratie" ] ]
+
+                --                , Html.li
+                --                    [ class "nav-item" ]
+                --                    [ Html.a [ class "nav-link", href (getPath (ViewDay "2019-01-01")) ] [ text "Gister" ] ]
+                --                , Html.li
+                --                    [ class "nav-item" ]
+                --                    [ Html.a [ class "nav-link", href (getPath (ViewDay "2019-01-02")) ] [ text "Vandaag" ] ]
+                , Html.li
+                    [ class "nav-item" ]
+                    [ Html.a [ class "nav-link", href (getPath SelectAvailableDay) ] [ text "Selecteer dag" ] ]
+                ]
+            ]
+        ]
+
+
+view : Model -> Browser.Document Msg
 view model =
-    case model of
+    let
+        header =
+            [ viewNavBar ]
+    in
+    case model.page of
+        UploadingDatabase database ->
+            { title = "Upload uren registratie", body = header ++ [ uploadView database ] }
+
+        Day day ->
+            { title = "Uren voor " ++ day, body = header ++ [ div [] [ text day ] ] }
+
+        AvailableDays ->
+            { title = "Kies een dag", body = [ div [] [ text "pick a day" ] ] }
+
+        PageNone ->
+            { title = "404", body = [ div [] [ text "Pagina niet gevonden" ] ] }
+
+
+uploadView : DatabaseState -> Html Msg
+uploadView database =
+    case database of
         Waiting ->
             input
                 [ type_ "file"
@@ -153,6 +303,7 @@ viewDuration duration =
         formattedMinutes =
             if String.length minutes == 1 then
                 "0" ++ minutes
+
             else
                 minutes
 
@@ -182,7 +333,7 @@ viewActions ( employee, actions ) =
 
 viewDay : ( String, WorkDay ) -> Html msg
 viewDay ( day, workDay ) =
-    Html.tr [ class "day-row"]
+    Html.tr [ class "day-row" ]
         [ Html.td [ class "day-title" ] [ text day ]
         , Html.td []
             [ Html.table [ class "employee" ]
